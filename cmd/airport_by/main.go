5833aad29354f93fb06d89cedca1f6115c5654f9
Denis Sipakov <mc.flyer@gmail.com>
2021-05-22 19:17:27 +0300

    Separate today tomorrow and yesterday

--- cmd/airport_by/main.go
+++ cmd/airport_by/main.go
@@ -33,6 +33,9 @@ func main() {
 
   var arrivals []models.AirportArrival
   var departures []models.AirportDepartureNow
+  var yesterdayArrivals []models.AirportArrivalYesterday
+  var todayArrivals []models.AirportArrivalToday
+  var tomorrowArrival []models.AirportArrivalTomorrow
 
   if err := json.NewDecoder(arrivalResponse.Body).Decode(&arrivals); err != nil {
     panic(err)
@@ -66,13 +69,9 @@ func main() {
     panic(err)
   }
 
-  if err := client.NewRef("arrivals").Set(ctx, arrivals); err != nil {
-    panic(err)
-  }
-
-  if err := client.NewRef("departures").Set(ctx, departures); err != nil {
-    panic(err)
-  }
+  // if err := client.NewRef("departures").Set(ctx, departures); err != nil {
+  //   panic(err)
+  // }
 
   layout := time.RFC3339
   plan := "2021-05-18T05:45:00+03:00"
@@ -83,10 +82,40 @@ func main() {
     panic(err)
   }
 
-  timeNow := time.Now()
-  fmt.Println(timeNow)
+  timeNow := time.Now() 
+
+  for _, v := range arrivals {
+    localTime, err := time.Parse(layout, v.PlannedTime)
+    if err == nil {
+      if localTime.Day() == timeNow.Add(-24 * time.Hour).Day() {
+        yesterdayArrivals = append(yesterdayArrivals, models.AirportArrivalYesterday{v})
+      } else if localTime.Day() == timeNow.Day() {
+        todayArrivals = append(todayArrivals, models.AirportArrivalToday{v})
+      } else if localTime.Day() == timeNow.Add(24 * time.Hour).Day() {
+        tomorrowArrival = append(tomorrowArrival, models.AirportArrivalTomorrow{v})
+      }
+    } 
+    
+  }
+
+  if err := client.NewRef("arrivals").Set(ctx, ""); err != nil {
+    panic(err)
+  }
+
+  if err := client.NewRef("arrivals/yesterday").Set(ctx, yesterdayArrivals); err != nil {
+    panic(err)
+  }
+
+  if err := client.NewRef("arrivals/today").Set(ctx, todayArrivals); err != nil {
+    panic(err)
+  }
+
+  if err := client.NewRef("arrivals/tomorrow").Set(ctx, tomorrowArrival); err != nil {
+    panic(err)
+  }
+
   fmt.Println(t)
-  fmt.Println(timeNow.Add(-24*time.Hour))
+  fmt.Println(timeNow.Add(-96*time.Hour).Day() == t.Day())
 
 
   fmt.Println(sb)
