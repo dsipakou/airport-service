51a07e47008bf88d9033b9f07908f62e99c8485a
Denis Sipakov <mc.flyer@gmail.com>
2021-06-08 12:48:17 +0300

    Ref

--- cmd/airport_by/main.go
+++ cmd/airport_by/main.go
@@ -2,18 +2,9 @@ package main
 
 import (
   // "fmt"
-  
   // "io/ioutil"
-  
-  
-
-  
-
-  
   //"firebase.google.com/go/auth"
-
   //"google.golang.org/api/option"
-  
   "github.io/dsipakou/airport-service/api/server/arrivals"
   "github.io/dsipakou/airport-service/api/server/departures"
 )
